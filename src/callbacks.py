64a4515f0e5c286261affb36b38baa49fe0155c9
Tezikov Roman <tez.romach@gmail.com>
2019-03-17 17:03:35 +0300

    EmbeddingsLossCallback refactor

--- src/callbacks.py
+++ src/callbacks.py
@@ -1,19 +1,31 @@
 import torch
 from catalyst.dl.callbacks import Callback
+from catalyst.dl.state import RunnerState
 
 
 class EmbeddingsLossCallback(Callback):
-    def __init__(self, emb_l2_reg=-1):
+    def __init__(
+        self,
+        emb_l2_reg: int = -1,
+        input_key: str = "targets",
+        embeddings_key: str = "embeddings",
+        logits_key: str = "logits",
+    ):
         self.emb_l2_reg = emb_l2_reg
+        self.embeddings_key = embeddings_key
+        self.logits_key = logits_key
+        self.input_key = input_key
 
-    def on_batch_end(self, state):
-        embeddings = state.output["embeddings"]
-        logits = state.output["logits"]
+    def on_batch_end(self, state: RunnerState):
+        embeddings = state.output[self.embeddings_key].float()
+        logits = state.output[self.logits_key].float()
 
-        loss = state.criterion(logits.float(), state.input["targets"].long())
+        targets = state.input[self.input_key].long()
+
+        loss = state.criterion(logits, targets)
 
         if self.emb_l2_reg > 0:
-            emb_loss = torch.mean(torch.norm(embeddings.float(), dim=1))
+            emb_loss = torch.mean(torch.norm(embeddings, dim=1))
             loss += emb_loss * self.emb_l2_reg
 
         state.loss = loss
