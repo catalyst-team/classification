b627b5e153033187d51e9c6ffe8ef50334371486
Sergey Kolesnikov <scitator@gmail.com>
2019-04-19 10:56:06 +0300

    refactored, simplified, merged with catalyst

--- src/__init__.py
+++ src/__init__.py
@@ -3,12 +3,8 @@ from catalyst.dl import registry
 
 from .experiment import Experiment
 from .runner import ModelRunner as Runner
-from .callbacks import EmbeddingsLossCallback, AUCCallback, \
-    ConfusionMatrixCallback, ConfusionMatrixCallbackV2
+from .callbacks import EmbeddingsLossCallback
 from .model import MultiHeadNet
 
 registry.Model(MultiHeadNet)
 registry.Callback(EmbeddingsLossCallback)
-registry.Callback(AUCCallback)
-registry.Callback(ConfusionMatrixCallback)
-registry.Callback(ConfusionMatrixCallbackV2)
