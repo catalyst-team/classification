c648c0ca6b100ba19dbbea50fa0d00da1683679a
Sergey Kolesnikov <scitator@gmail.com>
2019-09-16 14:14:05 +0300

    Update runner.py

--- src/runner.py
+++ src/runner.py
@@ -1,7 +1,7 @@
 try:
     import os
 
-    if os.environ.get("USE_WANDB", "0") == "1":
+    if os.environ.get("USE_WANDB", "1") == "1":
         from catalyst.dl import SupervisedWandbRunner as Runner
     else:
         from catalyst.dl import SupervisedRunner as Runner
