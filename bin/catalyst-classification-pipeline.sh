d3f071af453e551a053c48906d08dacb79fbafb4
Yauheni Kachan <19803638+bagxi@users.noreply.github.com>
2020-02-01 20:58:35 +0300

    update cli, fix -s flag

--- bin/catalyst-classification-pipeline.sh
+++ bin/catalyst-classification-pipeline.sh
@@ -7,29 +7,39 @@
 #version         :19.09.2
 #==============================================================================
 
-# usage:
-# WORKDIR=/path/to/logdir \
-# DATADIR=/path/to/dataset \
-# MAX_IMAGE_SIZE=... \  # 224 or 448 works good
-# BALANCE_STRATEGY=... \  # images in epoch per class, 1024 works good
-# CONFIG_TEMPLATE=... \ # model config to use
-# CRITERION=... \ # criterion
-# ./bin/catalyst-classification-pipeline.sh
+usage()
+{
+    cat << USAGE >&2
+Usage: $(basename $0) [OPTION...] [catalyst-dl run args...]
 
-# example:
-# CUDA_VISIBLE_DEVICES=0 \
-# CUDNN_BENCHMARK="True" \
-# CUDNN_DETERMINISTIC="True" \
-# WORKDIR=./logs \
-# DATADIR=./data/origin \
-# MAX_IMAGE_SIZE=224 \  # 224 or 448 works good
-# IMG_EXTENSION=jpg \
-# BALANCE_STRATEGY=256 \  # images in epoch per class, 1024 works good
-# CONFIG_TEMPLATE=./configs/templates/main.yml \
-# NUM_WORKERS=4 \
-# BATCH_SIZE=256 \
-# CRITERION=FocalLossMultiClass \
-# ./bin/catalyst-classification-pipeline.sh
+  -s, --skipdata                       Skip data preparation
+  -j, --num-workers NUM_WORKERS        Number of data loading/processing workers
+  -b, --batch-size BATCH_SIZE          Mini-batch size
+  --max-image-size MAX_IMAGE_SIZE      Target size of images e.g. 224, 448
+  --balance-strategy BALANCE_STRATEGY  Images in epoch per class, e.g. 1024
+  -m, --traced-method TRACED_METHOD    Model method to trace
+  --config-template CONFIG_TEMPLATE    Model config to use
+  --criterion CRITERION                Criterion to use
+  --datadir DATADIR
+  --workdir WORKDIR
+  catalyst-dl run args                 Execute \`catalyst-dl run\` with args
+
+Example:
+  CUDA_VISIBLE_DEVICES=0 \\
+  CUDNN_BENCHMARK="True" \\
+  CUDNN_DETERMINISTIC="True" \\
+  ./bin/catalyst-classification-pipeline.sh \\
+    --workdir ./logs \\
+    --datadir ./data/origin \\
+    --max-image-size 224 \\
+    --balance-strategy 256 \\
+    --config-template ./configs/templates/main.yml \\
+    --num-workers 4 \\
+    --batch-size 256 \\
+    --criterion FocalLossMultiClass
+USAGE
+    exit 1
+}
 
 set -e
 
@@ -56,50 +66,68 @@ set -e
 
 # ---- environment variables
 
-if [[ -z "$NUM_WORKERS" ]]; then
-      NUM_WORKERS=4
-fi
-
-if [[ -z "$BATCH_SIZE" ]]; then
-      BATCH_SIZE=64
-fi
-
-if [[ -z "$MAX_IMAGE_SIZE" ]]; then
-      MAX_IMAGE_SIZE=224
-fi
-
-if [[ -z "$IMG_EXTENSION" ]]; then
-      IMG_EXTENSION="jpg"
-fi
-
-if [[ -z "$BALANCE_STRATEGY" ]]; then
-      BALANCE_STRATEGY="null"
-fi
-
-if [[ -z "$TRACED_METHOD" ]]; then
-      TRACED_METHOD="forward_class"
-fi
-
-if [[ -z "$CONFIG_TEMPLATE" ]]; then
-      CONFIG_TEMPLATE="./configs/templates/main.yml"
-fi
-
-if [[ -z "$CRITERION" ]]; then
-        CRITERION="FocalLossMultiClass" # BCEWithLogits , CrossEntropyLoss
-fi
-
-if [[ -z "$DATADIR" ]]; then
-      DATADIR="./data/origin"
-fi
-
-if [[ -z "$WORKDIR" ]]; then
-      WORKDIR="./logs"
-fi
-
+NUM_WORKERS=${NUM_WORKERS:=4}
+BATCH_SIZE=${BATCH_SIZE:=64}
+MAX_IMAGE_SIZE=${MAX_IMAGE_SIZE:=224}
+BALANCE_STRATEGY=${BALANCE_STRATEGY:="null"}
+TRACED_METHOD=${TRACED_METHOD:="forward_class"}
+CONFIG_TEMPLATE=${CONFIG_TEMPLATE:="./configs/templates/main.yml"}
+CRITERION=${CRITERION:="FocalLossMultiClass"}  # BCEWithLogits , CrossEntropyLoss
+DATADIR=${DATADIR:="./data/origin"}
+WORKDIR=${WORKDIR:="./logs"}
 SKIPDATA=""
-while getopts ":s" flag; do
-  case "${flag}" in
-    s) SKIPDATA="true" ;;
+
+# bash argparse
+_parameters=""
+while (( "$#" )); do
+  case $1 in
+    -h|--help)
+      usage
+      ;;
+    -s|--skipdata)
+      SKIPDATA="true"
+      shift
+      ;;
+    -j|--num-workers)
+      NUM_WORKERS=$2
+      shift 2
+      ;;
+    -b|--batch-size)
+      BATCH_SIZE=$2
+      shift 2
+      ;;
+    --max-image-size)
+      MAX_IMAGE_SIZE=$2
+      shift 2
+      ;;
+    --balance-strategy)
+      BALANCE_STRATEGY=$2
+      shift 2
+      ;;
+    -m|--traced-method)
+      TRACED_METHOD=$2
+      shift 2
+      ;;
+    --config-template)
+      CONFIG_TEMPLATE=$2
+      shift 2
+      ;;
+    --criterion)
+      CRITERION=$2
+      shift 2
+      ;;
+    --datadir)
+      DATADIR=$2
+      shift 2
+      ;;
+    --workdir)
+      WORKDIR=$2
+      shift 2
+      ;;
+    *)
+      _parameters="${_parameters} $1"
+      shift
+      ;;
   esac
 done
 
@@ -127,7 +155,6 @@ if [[ -z "${SKIPDATA}" ]]; then
         --out-dir $IMAGES_DIR \
         --num-workers $NUM_WORKERS \
         --max-size $MAX_IMAGE_SIZE \
-        --extension $IMG_EXTENSION \
         --clear-exif
 
     catalyst-data tag2label \
@@ -164,7 +191,7 @@ cp -r ./configs/_common.yml $CONFIG_DIR/_common.yml
 
 catalyst-dl run \
     -C $CONFIG_DIR/_common.yml $CONFIG_DIR/config.yml \
-    --logdir $LOGDIR $*
+    --logdir $LOGDIR ${_parameters}
 
 
 # ---- model tracing
